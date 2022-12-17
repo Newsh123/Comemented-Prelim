b9f73d9d54a7260636326928945d086b0bbae743
Newsh123 <alex.newsh@gmail.com>
2022-12-17 11:00:31 +0000

    Add project files.

---
+++ Prelim Material/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prelim_Material
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
